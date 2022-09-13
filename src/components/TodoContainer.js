80107d8af30f46b0f12648e27011506085055bfb
[Adam] <[adam7305@elev.techcollege.dk]>
2022-09-13 21:27:40 +0200

    pokeapi setup

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,13 @@
+import React from "react"
+class TodoContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello! This is a list!</h1>
+                <p>A To Do kinda list!</p>
+            </div>
+        )
+    }
+}
+
+export default TodoContainer
--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from "react"
+import "./style.css"
+const ReactDOM = require('react-dom/client');
+
+const App = () => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [search, setSearch] = useState({
+        slug: "",
+    });
+
+    const handleChange = (event) => {
+        if (event.key === 'Enter') {
+            setSearch(({...search, slug: event.target.value}))
+            console.log("Search: " + event.target.value)
+        }
+    }
+
+    //Fetch
+    useEffect(() => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${search.slug}`)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`HTTP Error: ${response.status}`);
+                }
+                return response.json()
+            })
+            .then((actualData) => {
+                if(search.slug !== "") {
+                    setData(actualData);
+                    setError(null);
+                }
+            })
+            .catch((err) => {
+                setError(err.message);
+                setData(null);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, [search.slug]);
+
+    console.log(data)
+
+    return <div className="App">
+        <h1>Search for 'mons!</h1>
+        {loading && <p>Just a moment!</p>}
+        {error && (
+            <div>
+                <p>{`Couldn't find`} <strong>${search.slug}</strong> {`. Try again.`}</p>
+                <p>{`Error: ${error}`}</p>
+            </div>
+        )}
+        <input type="text" id="pokemonInput" onKeyDown={handleChange} defaultValue={""}/>
+
+        {data && (
+            <div>
+                <h1>{data.name}</h1>
+                <img src={Object.values(data.sprites.other['official-artwork'])[0].toString()} alt="test"/>
+                <p>{data.id}</p>
+            </div>
+        )}
+    </div>
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+)
